f40eaba78871fb84f719511c9dfc49b3420568a8
angie_palacios <adsi2.co@longportaviation.com>
2020-01-10 15:35:17 -0500

    [ADD] Add controller profiles and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  post '/profiles/:id', :to => 'profiles#destroy', :as => :user
 
   get 'home/index'
+
+  resources :profiles
 
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
