f89548e93f154ff84d4eed8de62bfc8c377e9f0b
angie_palacios <adsi2.co@longportaviation.com>
2020-01-13 11:18:25 -0500

    [UPD] Update of permissions in the users types

---
+++ db/migrate/20200113160658_create_join_table_permissions_rol.rb
@@ -0,0 +1,8 @@
+class CreateJoinTablePermissionsRol < ActiveRecord::Migration[5.1]
+  def change
+    create_join_table :permissions, :rols do |t|
+      t.references :permission, foreign_key: true
+      t.references :rols, foreign_key: true
+    end
+  end
+end
